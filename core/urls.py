f0859279f7565849c9c7e99e2485bd1b91dc8084
Andrii Yashyn <andrii_yashyn@epam.com>
2018-02-25 22:11:28 +0200

    - Add organizations page - Add teachers page

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,26 @@
-from django.conf.urls import url, include
+from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from .views import OrganizationList, TeacherList
+from core.subviews.organization_views import OrganizationList, EditOrganization, DeleteOrganization, SaveOrganization, \
+    CreateOrganization
+from core.subviews.teacher_views import EditTeacher, DeleteTeacher, SaveTeacher, CreateTeacher, TeacherList
 
 urlpatterns = {
-    url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'', OrganizationList.as_view(), name='Organizations'),
-    url(r'^organizations/', OrganizationList.as_view(), name='Organizations'),
-    url(r'^teachers/', TeacherList.as_view(), name='Teachers'),
+    path('', OrganizationList.as_view(), name='Organizations'),
+    path('organization/edit/<int:id>', EditOrganization.as_view(), name='EditOrganization'),
+
+    path('organization/delete/<int:id>', DeleteOrganization.as_view(), name='DeleteOrganization'),
+    path('organization/save/<int:id>', SaveOrganization.as_view(), name='SaveOrganization'),
+    path('organization/save/', SaveOrganization.as_view(), name='SaveOrganization'),
+    path('organization/add', CreateOrganization.as_view(), name='CreateOrganization'),
+    path('organizations/', OrganizationList.as_view(), name='Organizations'),
+
+    path('teacher/edit/<int:id>', EditTeacher.as_view(), name='EditTeacher'),
+    path('teacher/delete/<int:id>', DeleteTeacher.as_view(), name='EditTeacher'),
+    path('teacher/save/<int:id>', SaveTeacher.as_view(), name='SaveTeacher'),
+    path('teacher/save/', SaveTeacher.as_view(), name='SaveTeacher'),
+    path('teacher/add', CreateTeacher.as_view(), name='UpdateTeacher'),
+    path('teachers/', TeacherList.as_view(), name='Teachers'),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
